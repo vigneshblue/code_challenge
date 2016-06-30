e53d5ff52c934f9ee97bb11f2a921b02985dc3fc
vigneshblue <mynanjal@gmail.com>
2016-06-30 19:13:09 +0530

    Resolving Deployment error

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -19,8 +18,7 @@ ActiveRecord::Schema.define(version: 20160627094842) do
     t.string   "reporting_manager"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
+    t.index ["name"], name: "index_Employee_Details_on_name", unique: true
   end
-
-  add_index "employee_details", ["name"], name: "index_Employee_Details_on_name", unique: true
 
 end
