cfac539d17abf33bf6a713528818db7fa828498b
vigneshblue <mynanjal@gmail.com>
2016-07-02 09:38:43 +0530

    Implemented self join technique for table

--- app/models/employee_detail.rb
+++ app/models/employee_detail.rb
@@ -1,4 +1,6 @@
 class EmployeeDetail < ActiveRecord::Base
 	validates :name, presence: true, uniqueness: { case_sensitive: false }
 	validates :designation, presence: true
+	belongs_to :reporting_details, :class_name => "EmployeeDetail", :foreign_key => "reporting_manager"
+
 end
